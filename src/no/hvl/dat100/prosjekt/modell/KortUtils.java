5cd9ea3f2cc3c531ac79846c2b810928b0f96585
h584903 <584903@stud.hvl.no>
2021-10-19 14:37:27 +0200

    Sorter skal nå være ferdig

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -20,16 +20,51 @@ public class KortUtils {
 	public static void sorter(KortSamling samling) {
 
 
-		Kort[] bunke = samling.getAllekort();
+		Kort[] bunke = samling.getAllekort(); // Henter inn kortene ;PT
 
+		/*
+		Først sorterer den samlingen utifra farge
+
+		;PT
+		 */
+
+		for (int i = 0; i < samling.getAntalKort()-1; i++){
+			if (!bunke[i].sammeFarge(bunke[i+1])){
+				int antall = 0;
+
+				for (int j = 0; j < samling.getAntalKort(); j++){
+					if (bunke[i].sammeFarge(bunke[j]) && j != i){
+						Kort temp = bunke[i+1];
+						bunke[i+1] = bunke[j];
+						bunke[j] = temp;
+						antall++;
+					}
+				}
+			}
+		}
+
+
+		/*
+		Sorterer så ved å sammenligne det første kortet med verdien til det andre
+		hvis det første kortets verdi er størst, så vil de bytte plass, dette gjøres bare hvis de er samme farge
+
+		;PT
+		 */
 		for (int i = 0; i < samling.getAntalKort(); i++){
 			boolean likFarge = false;
 			if (bunke[i].sammeFarge(bunke[i+1])){
-				bunke[i].compareTo(bunke[samling.getAntalKort()]);
-
-
+				Kort temp;
+				if (bunke[i].compareTo(bunke[i+1]) > 0){
+					temp = bunke[i+1];
+					bunke[i+1] = bunke[i];
+					bunke[i] = temp;
+				}
 			}
 		}
+
+		samling.fjernAlle();
+		for (Kort kort : bunke)
+			samling.leggTil(kort);
 	}
 		
 //	 Arrays.sort(samling);
