23a16bc4eabc360c4ab20199eb613f85bc5115af
henning <henning@localhost>
2021-10-20 21:43:00 +0200

    Oppgave 1 ferdig

--- src/no/hvl/dat100/prosjekt/modell/KortSamling.java
+++ src/no/hvl/dat100/prosjekt/modell/KortSamling.java
@@ -7,101 +7,100 @@ public class KortSamling {
 	private final int MAKS_KORT = 4 * Regler.MAKS_KORT_FARGE;
 
 	private Kort[] samling;
-	
+
 	private int antall;
-	
+
 	public KortSamling() {
-		
+
 		samling = new Kort[MAKS_KORT];
-		
+
 	}
 
 	public Kort[] getSamling() {
-		
+
 		return samling;
 	}
-	
+
 	public int getAntalKort() {
-		
+
 		return antall;
-		
+
 	}
-	
+
 	public boolean erTom() {
-		
+
 		return antall == 0;
 	}
 
 	public void leggTil(Kort kort) {
-		
-		if(kort == null) {
+
+		if (kort == null) {
 			return;
 		}
-		for(int i = 0; i < samling.length; i++)
-			if(samling[i] == null) {
+		for (int i = 0; i < samling.length; i++)
+			if (samling[i] == null) {
 				samling[i] = kort;
 				antall++;
 				return;
-		    }
+			}
 	}
-	
+
 	public void leggTilAlle() {
-	
+
 		for (Kortfarge kortfarge : Kortfarge.values()) {
-			for (int i = 1; i <= Regler.MAKS_KORT_FARGE ; i++) {
+			for (int i = 1; i <= Regler.MAKS_KORT_FARGE; i++) {
 				leggTil(new Kort(kortfarge, i));
 			}
 		}
 	}
-	
+
 	public void fjernAlle() {
-		
+
 		samling = new Kort[MAKS_KORT];
 		antall = 0;
-		
-	
+
 	}
-	
+
 	public Kort seSiste() {
-		
-		for(int i = samling.length-1; i >= 0 ; i--) {
-			if(samling[i] != null) {
+
+		for (int i = samling.length - 1; i >= 0; i--) {
+			if (samling[i] != null) {
 				return samling[i];
-			}		
+			}
 		}
 		return null;
 	}
 
 	public Kort taSiste() {
-		
-		for(int i = samling.length-1; i >= 0 ; i--) {
-			if(samling[i] != null){
+
+		for (int i = samling.length - 1; i >= 0; i--) {
+			if (samling[i] != null) {
 				final Kort siste = samling[i];
 				samling[i] = null;
 				antall--;
 				return siste;
-			}		
+			}
 		}
 		return null;
 	}
-	
+
 	public boolean har(Kort kort) {
 
-		for (int i = 0; i<samling.length; i++) {
+		for (int i = 0; i < samling.length; i++) {
 			if (samling[i] != null && samling[i].equals(kort)) {
 				return true;
 			}
 		}
 		return false;
-		
+
 	}
-			 
+
 	public boolean fjern(Kort kort) {
-		
-		if(kort == null) {
+
+		if (kort == null) {
 			return false;
 		}
-		for (int i = 0; i<samling.length; i++) {
+		for (int i = 0; i < samling.length; i++) {
 			if (samling[i] == kort) {
 				samling[i] = null;
 				antall--;
@@ -112,16 +111,16 @@ public class KortSamling {
 	}
 
 	public Kort[] getAllekort() {
-				
-		final Kort [] alleKort = new Kort[antall];
-		
+
+		final Kort[] alleKort = new Kort[antall];
+
 		int k = 0;
-		for (int j = 0; j<samling.length; j++) {
-			if(samling[j] != null) {
+		for (int j = 0; j < samling.length; j++) {
+			if (samling[j] != null) {
 				alleKort[k] = samling[j];
 				k++;
 			}
 		}
 		return alleKort;
-	}	
+	}
 }
